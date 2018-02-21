91be5087fa6a9d167f651530300f5706d3faf81e
dshung1997 <dshung1997@gmail.com>
2018-02-21 07:43:45 +0700

    Update

---
+++ Assets/Script/BackGround.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackGround : MonoBehaviour {
+
+    public float speed;
+    // Use this for initialization
+	void Start () { }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.Translate(Vector3.left * speed * Time.deltaTime);
+    }
+    public int numBGPanels = 6;
+    //Sao cái này lại bằng 6?
+
+    void OnTriggerEnter2D(Collider2D collider)
+    /* Collider2D: được sử dụng để xác định hình dạng vật lý của một đối tượng
+     * Có thể tham gia trong va chạm 2D và kích hoạt các sự kiện
+     */
+    {
+
+        if (collider.name == "BGLooper") /*Nếu chạm trúng thằng có tên là BGLooper*/
+        {
+            float widthOfBGObject = ((BoxCollider2D)GetComponent<Collider2D>()).size.x - 0.01f;
+            // biến widthOfBGObject này là để làm cđg?
+            Vector3 pos = this.transform.position;
+
+            pos.x += widthOfBGObject * numBGPanels;
+            this.transform.position = pos;
+        }
+    }
+}
---
+++ Assets/Script/BirdMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdMovement : MonoBehaviour {
+
+    // Use this for initialization
+    private Animator _animator;
+    private void Start()
+    {
+        _animator = transform.GetComponentInChildren<Animator>();
+    }
+    public float jumpVelocity;
+    float g = -9.8f;
+
+    public bool dead;
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        _animator.SetTrigger("Death");
+        dead = true;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        // x = x0 + v0 * t + a * t * t / 2
+        transform.position += Vector3.up * (jumpVelocity * Time.deltaTime + g * Time.deltaTime * Time.deltaTime / 2);
+
+        // vt = v0 + a * t
+        jumpVelocity += g * Time.deltaTime;
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
+        {
+            _animator.SetTrigger("DoFlap");
+            jumpVelocity = 2.5f;
+        }
+        if (dead) return;
+    }
+}
